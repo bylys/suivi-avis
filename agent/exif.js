622997cae986e0d8b91298bebb578883bf09b520
bylys <m.payot76@gmail.com>
2026-08-20 12:22:50 +0700

    Repartition ponderee des smartphones (50% Apple iPhone, 40% Samsung Galaxy, 10% Xiaomi/OPPO/Pixel/LG)

--- agent/exif.js
+++ agent/exif.js
@@ -5,14 +5,41 @@
 const piexif = require('piexifjs');
 const { getCoordinatesForCity } = require('./geocoding');
 
-const SMARTPHONE_MODELS = [
+const IPHONE_MODELS = [
+  { make: 'Apple', model: 'iPhone 13', software: '16.5', focalLength: [51, 10] },
+  { make: 'Apple', model: 'iPhone 14', software: '16.6', focalLength: [57, 10] },
+  { make: 'Apple', model: 'iPhone 14 Pro', software: '17.0', focalLength: [68, 10] },
+  { make: 'Apple', model: 'iPhone 15', software: '17.2', focalLength: [60, 10] },
+];
+
+const SAMSUNG_MODELS = [
+  { make: 'Samsung', model: 'Galaxy S21', software: 'G991BXXU5EWH1', focalLength: [54, 10] },
   { make: 'Samsung', model: 'Galaxy S22', software: 'S901BXXU2BVJA', focalLength: [54, 10] },
   { make: 'Samsung', model: 'Galaxy S23', software: 'S911BXXU1AWA6', focalLength: [54, 10] },
-  { make: 'Apple', model: 'iPhone 13', software: '16.5', focalLength: [51, 10] },
-  { make: 'Apple', model: 'iPhone 14 Pro', software: '16.6', focalLength: [68, 10] },
-  { make: 'Xiaomi', model: 'Redmi Note 11', software: 'MIUI 13.0.5', focalLength: [43, 10] },
-  { make: 'Google', model: 'Pixel 7', software: 'TQ2A.230505.002', focalLength: [68, 10] },
+  { make: 'Samsung', model: 'Galaxy A53 5G', software: 'A536BXXU4CWA2', focalLength: [52, 10] },
 ];
+
+const OTHER_MODELS = [
+  { make: 'Xiaomi', model: 'Redmi Note 11', software: 'MIUI 13.0.5', focalLength: [43, 10] },
+  { make: 'Xiaomi', model: '12T Pro', software: 'MIUI 14.0.1', focalLength: [50, 10] },
+  { make: 'OPPO', model: 'Reno8 5G', software: 'ColorOS 13', focalLength: [48, 10] },
+  { make: 'Google', model: 'Pixel 7', software: 'TQ2A.230505.002', focalLength: [68, 10] },
+  { make: 'LG', model: 'VELVET 5G', software: 'G900N30a', focalLength: [47, 10] },
+];
+
+function pickSmartphone() {
+  const rand = Math.random();
+  if (rand < 0.50) {
+    // 50% iPhone
+    return IPHONE_MODELS[Math.floor(Math.random() * IPHONE_MODELS.length)];
+  } else if (rand < 0.90) {
+    // 40% Samsung
+    return SAMSUNG_MODELS[Math.floor(Math.random() * SAMSUNG_MODELS.length)];
+  } else {
+    // 10% Autres marques (Xiaomi, Oppo, Google Pixel, LG)
+    return OTHER_MODELS[Math.floor(Math.random() * OTHER_MODELS.length)];
+  }
+}
 
 function degToDmsRational(degFloat) {
   const absolute = Math.abs(degFloat);
@@ -31,8 +58,7 @@ function degToDmsRational(degFloat) {
 async function injectExifAndGps(imageBuffer, cityName, country = 'France') {
   try {
     const coords = await getCoordinatesForCity(cityName, country);
-    const pick = (arr) => arr[Math.floor(Math.random() * arr.length)];
-    const phone = pick(SMARTPHONE_MODELS);
+    const phone = pickSmartphone();
 
     const now = new Date();
     const pad = (n) => String(n).padStart(2, '0');
